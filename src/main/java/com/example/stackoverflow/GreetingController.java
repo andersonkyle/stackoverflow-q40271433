bb3a49366510421a5f8071342de98a58c9a1a609
Kyle Anderson <kanderson450@users.noreply.github.com>
2016-10-27 15:46:17 -0700

    Instead of setting a default value in the RequestParam, you could pull it from a properties file albeit with more code...

--- src/main/java/com/example/stackoverflow/GreetingController.java
+++ src/main/java/com/example/stackoverflow/GreetingController.java
@@ -2,6 +2,7 @@ package com.example.stackoverflow;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +13,12 @@ public class GreetingController {
 	
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	@Value("${my.setting}")
+	private String dooDadDefault;
 
 	@RequestMapping(value = "/greeting", method = RequestMethod.POST)
-	public void greeting(@RequestParam(value = "name", defaultValue = "World") String name, @RequestParam(value = "dooDad", defaultValue = "bar") String dooDad){
+	public void greeting(@RequestParam(value = "name", defaultValue = "World") String name, @RequestParam(value = "dooDad", required = false) String dooDad){
+		dooDad = dooDad == null ? dooDadDefault : dooDad; 
 		Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, name), dooDad);
 		System.out.println(greeting);
 	}
